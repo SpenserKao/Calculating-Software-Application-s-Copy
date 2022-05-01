e0b36b791df5dae9c35a5ef44a134b106041c888
Spenser Kao <SpenserKao@Optusnet.com.au>
2022-05-02 02:07:38 +1000

    Add files via upload
    First draft of package application.go.

--- src/application/application.go
+++ src/application/application.go
@@ -1,1 +1,58 @@
+package application
 
+import (
+	"errors"
+)
+
+type ComputerTypeCount struct {
+	LaptopCount  int
+	DesktopCount int
+}
+
+type application struct {
+	m map[string]interface{}
+}
+
+func NewUsage() *application {
+	a := &application{}
+	a.m = make(map[string]interface{})
+	return a
+}
+
+func (a *application) Contains(key string) bool {
+	_, c := a.m[key]
+	return c
+}
+
+func (a *application) GetVal(key string) (interface{}, error) {
+	i, ok := a.m[key]
+	if ok {
+		return i, nil
+	} else {
+		return nil, errors.New("Key '" + key + "' NOT in entry ")
+	}
+}
+
+func (a *application) Add(key string, val interface{}) {
+	a.m[key] = val
+}
+
+func (a *application) CalculateCopyNumber() int {
+	copyTotalNo := 0
+	for _, app := range a.m {
+		var appx ComputerTypeCount
+		// Type Assertion, again.
+		appx.DesktopCount = app.(ComputerTypeCount).DesktopCount
+		appx.LaptopCount = app.(ComputerTypeCount).LaptopCount
+		copyTotalNo += sumUpOneUserID(appx)
+	}
+	return copyTotalNo
+}
+
+func sumUpOneUserID(a ComputerTypeCount) int {
+	if a.LaptopCount > 0 && a.DesktopCount > 0 {
+		return a.DesktopCount
+	} else {
+		return a.LaptopCount + a.DesktopCount
+	}
+}
